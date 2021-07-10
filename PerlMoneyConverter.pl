6b8446317b22181b96c81b7a6edd491a72c15c8f
MARKMENTAL <marky611@gmail.com>
2021-07-10 15:12:47 -0400

    Update PerlMoneyConverter.pl
    Added error handling for invalid formatting of money, program now restarts when non-numeric input is detected in the '$money' variable. Conversion rate also adjusted to the mid-market rate.

--- PerlMoneyConverter.pl
+++ PerlMoneyConverter.pl
@@ -1,32 +1,40 @@
 use warnings;
 use strict;
+use Scalar::Util qw(looks_like_number);
 
 my $datestring = localtime();
 
 print("Please enter your name:\n");
-my $name = <>;
+my $name       = <>;
 my $dollarsign = '$';
 chomp($name);
 print("Please enter the amount of USD to convert to Japanese Yen:\n");
+
 my $money = 0;
 $money = <>;
-my $moneyconverted = $money * 110.11;
+my $moneyconverted = $money * 110.058;
 chomp($money);
 
-my $str = <<END;
-$datestring 
-Hello $name, You have $dollarsign$money, which is ¥$moneyconverted 
+if ( looks_like_number($money) ) {
+    my $str = <<END;
+$datestring Hello $name, You have $dollarsign$money, which is ¥$moneyconverted 
 END
 
-my $filename = 'test3.txt';
+    my $filename = 'PerlMCRecords.txt';
 
-#fh is perl's filehandler object
-#two '>>' symbols are need to tell filehandler to append not delete
-open(FH, '>>', $filename) or die $!;
+    #fh is perl's filehandler object
+    #two '>>' symbols are need to tell filehandler to append not delete
+    open( FH, '>>', $filename ) or die $!;
+    print FH $str;
+    close(FH);
+    chomp($str);
+    print "Wrote||\n$str \n||to file successfully!\n";
+}
 
-print FH $str;
+else {
+    print "Invalid input. Program will restart \n";
 
-close(FH);
+#program self-referentially executes itself($0) from the command line here to restart
+    exec( $^X, $0, @ARGV );
+}
 
-chomp($str);
-print "Wrote||\n$str \n||to file successfully!\n";

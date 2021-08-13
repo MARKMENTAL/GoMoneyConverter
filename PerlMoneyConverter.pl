400ff867d92260b94a133ccea339bb73f562c881
MARKMENTAL <marky611@gmail.com>
2021-08-13 14:52:30 -0400

    Update PerlMoneyConverter.pl
    Added ability to convert to GBP

--- PerlMoneyConverter.pl
+++ PerlMoneyConverter.pl
@@ -7,17 +7,22 @@ my $datestring = localtime();
 print("Please enter your name:\n");
 my $name       = <>;
 my $dollarsign = '$';
+
+print ("Now enter the currency you'd like to convert to \n1 for JPY\n2 for GBP\n");
+my $currency = <>;
 chomp($name);
-print("Please enter the amount of USD to convert to Japanese Yen:\n");
 
-my $money = 0;
-$money = <>;
-my $moneyconverted = $money * 110.058;
-chomp($money);
+if ($currency == 1){
+    print("Please enter the amount of USD to convert to Japanese Yen:\n");
 
-if ( looks_like_number($money) ) {
+    my $money = 0;
+    $money = <>;
+    my $moneyconverted = $money * 110.058;
+    chomp($money);
+
+    if ( looks_like_number($money) ) {
     my $str = <<END;
-$datestring Hello $name, You have $dollarsign$money, which is ¥$moneyconverted 
+$datestring Hello $name, You have $dollarsign$money, which is ¥$moneyconverted
 END
 
     my $filename = 'PerlMCRecords.txt';
@@ -25,16 +30,44 @@ END
     #fh is perl's filehandler object
     #two '>>' symbols are need to tell filehandler to append not delete
     open( FH, '>>', $filename ) or die $!;
+    chomp($str);
     print FH $str;
     close(FH);
-    chomp($str);
-    print "Wrote||\n$str \n||to file successfully!\n";
+    print "Wrote\n$str\nto file successfully!\n";
+}
 }
 
-else {
+if ($currency == 2){
+    print("Please enter the amount of USD to convert to Pound Sterling:\n");
+
+    my $money = 0;
+    $money = <>;
+    my $moneyconverted = $money * 0.72095367;
+    chomp($money);
+
+    if ( looks_like_number($money) ) {
+    my $str = <<END;
+$datestring Hello $name, You have $dollarsign$money, which is £$moneyconverted
+END
+
+    my $filename = 'PerlMCRecords.txt';
+
+    #fh is perl's filehandler object
+    #two '>>' symbols are need to tell filehandler to append not delete
+    open( FH, '>>', $filename ) or die $!;
+    chomp($str);
+    print FH $str;
+    print FH "\n";
+    close(FH);
+    print "Wrote\n$str\nto file successfully!\n";
+}
+}
+
+if ($currency == 0) {
     print "Invalid input. Program will restart \n";
 
 #program self-referentially executes itself($0) from the command line here to restart
     exec( $^X, $0, @ARGV );
 }
+
 

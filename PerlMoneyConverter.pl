b3c8c82c124877d735178bf3cf03c75d5f91b9ce
mm666 <marky611@gmail.com>
2021-08-30 22:55:48 -0400

    added Indian Rupees as a currency option and cleaned up formatting of output

--- PerlMoneyConverter.pl
+++ PerlMoneyConverter.pl
@@ -7,8 +7,7 @@ my $datestring = localtime();
 print("Please enter your name:\n");
 my $name       = <>;
 my $dollarsign = '$';
-
-print ("Now enter the currency you'd like to convert to \n1 for JPY\n2 for GBP\n");
+print ("Now enter the currency you'd like to convert to \n1 for JPY\n2 for GBP\n3 for Euro\n4 for INR\n");
 my $currency = <>;
 chomp($name);
 
@@ -32,12 +31,15 @@ END
     open( FH, '>>', $filename ) or die $!;
     chomp($str);
     print FH $str;
+    print FH "\n";
     close(FH);
-    print "Wrote\n$str\nto file successfully!\n";
-}
+    print "\n\n$dollarsign$money is equal to £$moneyconverted\n\n";
+    print "****************\nFile Writing\n****************\nWrote\n$str\nto file successfully!\n\n";
+    }
+
 }
 
-if ($currency == 2){
+elsif ($currency == 2){
     print("Please enter the amount of USD to convert to Pound Sterling:\n");
 
     my $money = 0;
@@ -59,15 +61,69 @@ END
     print FH $str;
     print FH "\n";
     close(FH);
-    print "Wrote\n$str\nto file successfully!\n";
+    print "\n\n$dollarsign$money is equal to £$moneyconverted\n\n";
+    print "****************\nFile Writing\n****************\nWrote\n$str\nto file successfully!\n\n";
+    }
+
 }
+
+elsif ($currency == 3){
+    print("Please enter the amount of USD to convert to Euros:\n");
+
+    my $money = 0;
+    $money = <>;
+    my $moneyconverted = $money * 0.84775399;
+    chomp($money);
+
+    if ( looks_like_number($money) ) {
+    my $str = <<END;
+$datestring Hello $name, You have $dollarsign$money, which is €$moneyconverted
+END
+
+    my $filename = 'PerlMCRecords.txt';
+
+    #fh is perl's filehandler object
+    #two '>>' symbols are need to tell filehandler to append not delete
+    open( FH, '>>', $filename ) or die $!;
+    chomp($str);
+    print FH $str;
+    print FH "\n";
+    close(FH);
+    print "\n\n$dollarsign$money is equal to €$moneyconverted\n\n";
+    print "****************\nFile Writing\n****************\nWrote\n$str\nto file successfully!\n\n";
+}
+
+}
+
+elsif ($currency == 4){
+    print("Please enter the amount of USD to convert to Indian Rupees:\n");
+
+    my $money = 0;
+    $money = <>;
+    my $moneyconverted = $money * 73.29;
+    chomp($money);
+
+    if ( looks_like_number($money) ) {
+    my $str = <<END;
+$datestring Hello $name, You have $dollarsign$money, which is ₹$moneyconverted
+END
+
+    my $filename = 'PerlMCRecords.txt';
+
+    #fh is perl's filehandler object
+    #two '>>' symbols are needed to tell filehandler to append not delete
+    open( FH, '>>', $filename ) or die $!;
+    chomp($str);
+    print FH $str;
+    print FH "\n";
+    close(FH);
+    print "\n\n$dollarsign$money is equal to ₹$moneyconverted\n\n";
+    print "****************\nFile Writing\n****************\nWrote\n$str\nto file successfully!\n\n";
+    }
 }
 
 if ($currency == 0) {
     print "Invalid input. Program will restart \n";
-
 #program self-referentially executes itself($0) from the command line here to restart
     exec( $^X, $0, @ARGV );
 }
-
-
